a9c6f1b22a6b4af0df61334f1799f8d58fae4426
unknownrocks619 <unknownrocks619@hotmail.com>
2022-03-19 06:10:07 +0545

    bug fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,41 +25,4 @@
     </div>
   </section>
 <!-- End Welcome -->
-
-<!-- Personal Detail -->
-<!-- Start About -->
-<section class='about section background-parallax bg-image-2'>
-        <div class='overlay'></div>
-        <div class='container'>
-          <div class='row'>
-            <div class='col-sm-12'>
-              <div class='hero-content'>
-                <h1 class='section-title text-left animated' data-animation-delay='0' data-animation='fadeInUp'>
-                  Upload Application Form
-                </h1>
-              </div>
-              <div class='row'>
-                <div class='col-sm-6'>
-                  <div class='mg-b'>
-                    <p class='animated' data-animation-delay='100' data-animation='fadeInUp'>
-                      Before Starting your online application form submission please watch video on how to 
-                      upload online application form from below button.
-                    </p>
-                  </div>
-                  <a class='btn btn-outline btn-lg js-link-video animated' data-animation-delay='200' data-animation='fadeInUp' data-slide='4' href='http://www.youtube.com/watch?v=Zs8T-FCEvXg'>
-                    Watch Video
-                    <i class="fa fa-eye" aria-hidden="true"></i>
-                  </a>
-                  <a href="{{ route('personal_info') }}" class='btn btn-primary btn-lg js-link-video animated'>
-                    Start Application Form
-                    <i class="fa fa-play" aria-hidden="true"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <!-- End About -->
-<!-- End Personal Detail -->
 @endsection
